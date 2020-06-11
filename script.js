deae0d3482b242ffe929f83ff830f64da29ea49e
Vlad Mihet <vlad.mihet@yahoo.com>
2020-06-11 10:29:18 +0300

    fsd

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function getQuote () {
-    const json = require('./quotes.json');
+const json = require('./quotes.json');
+
+function getQuote() {
     randomQuote = Math.floor(Math.random() * json.length)
-    /* console.log(randomQuote);
-    console.log(json[randomQuote].quoteText + '\n' + json[randomQuote].quoteAuthor); */
+        /* console.log(randomQuote);
+        console.log(json[randomQuote].quoteText + '\n' + json[randomQuote].quoteAuthor); */
     author = document.getElementById('author');
     quote = document.getElementById('quote');
-    quote.innerText = '-' + json[randomQuote].quoteText;
+    quote.innerText = json[randomQuote].quoteText;
     author.innerText = json[randomQuote].quoteAuthor;
 }
 
@@ -15,7 +16,11 @@ function random_bg_color() {
     var z = Math.floor(Math.random() * 256);
     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
     document.body.style.background = bgColor;
+    document.getElementById('quoteDiv').style.color = bgColor;
 }
+
+var button = document.getElementById('nextQuoteButton');
+button.onclick = getQuote();
 
 random_bg_color();
 getQuote();
